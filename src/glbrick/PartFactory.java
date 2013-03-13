d4934b2ac470a52c5909ea7634ef43dddaa3e94a
John D <tanpantsman@gmail.com>
2013-03-13 22:10:43 +0100

    trying to fix prse problems.  apparently some colors are declared in binary or hex. wtf?

--- src/glbrick/PartFactory.java
+++ src/glbrick/PartFactory.java
@@ -1,7 +1,10 @@
 package glbrick;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class PartFactory {
@@ -9,13 +12,13 @@ public class PartFactory {
 	private ArrayList<String> paths;
 	private ColorBase colors;
 	private boolean printFailures;
-	
+
 	public PartFactory(String ldrawPath) throws FileNotFoundException {
 		paths = new ArrayList<String>();
 		parts = new TreeMap<String,PartSpec>();
 		colors = new ColorBase(ldrawPath);
 		printFailures = false;
-		
+
 		String partsPath = ldrawPath + File.separator + "parts";
 		String pPath = ldrawPath + File.separator + "p";
 
@@ -24,13 +27,13 @@ public class PartFactory {
 		addPath(partsPath + File.separator + "S");
 		addPath(pPath + File.separator + "48");
 	}
-	
+
 	public void activateFailureErrors() {
 		printFailures = true;
 	}
-	
+
 	public Collection<PartSpec> getAllParts() {return parts.values();}
-	
+
 	public void addPath(String path) {
 		paths.add(path);
 	}
@@ -58,49 +61,70 @@ public class PartFactory {
 
 		String printstr = "";
 		for (String s : strArr){
-		printstr += s + " ";	
+			printstr += s + " ";	
 		}
-		
+
 		return printstr;
 	}
 	private PartSpec tryFile(String path, String partName) throws FileNotFoundException, PartNotFoundException {
-		String filename = path + File.separator + brickUtilities.fixPath(partName);
-		Scanner s = new Scanner(new File(filename));
+		String fileName = path + File.separator + brickUtilities.fixPath(partName);
+
+		FileReader fr = new FileReader(fileName);
+		BufferedReader reader = new BufferedReader(fr);
+
+
+
+
+		String snl = "";
 		PartSpec result = new PartSpec(partName, colors);
-		while (s.hasNextLine()) {
-			String snl = s.nextLine();
-			//System.out.println(snl);
-			String[] lineParts = snl.split("\\s+");
-			if (lineParts.length > 0 && lineParts[0].length() > 0) {
-				int code = Integer.parseInt(lineParts[0]);
-				//System.out.println(mkString(lineParts));
-				if (code == 0) {
-					result.addLine(new CommentSpec(lineParts));
-				} else if (code == 1) {
-					result.addLine(new SubpartSpec(lineParts, this));
-				} else if (code == 2) {
-					result.addLine(new LineSpec(lineParts, colors));
-				} else if (code == 3) {
-					result.addLine(new TriangleSpec(lineParts, colors));
-				} else if (code == 4) {
-					try{
-					result.addLine(new QuadSpec(lineParts, colors));
+		try {
+			while (reader.ready()) {
+				snl = reader.readLine();
+				//System.out.println(snl);
+				String[] lineParts = snl.split("\\s+");
+				if (lineParts.length > 0 && lineParts[0].length() > 0) {
+					int code = Integer.parseInt(lineParts[0]);
+					//System.out.println(mkString(lineParts));
+					if (code == 0) {
+						result.addLine(new CommentSpec(lineParts));
+					} else if (code == 1) {
+						result.addLine(new SubpartSpec(lineParts, this));
+					} else if (code == 2) {
+						result.addLine(new LineSpec(lineParts, colors));
+					} else if (code == 3) {
+						result.addLine(new TriangleSpec(lineParts, colors));
+					} else if (code == 4) {
+						try{
+							result.addLine(new QuadSpec(lineParts, colors));
+						}
+						catch ( ArrayIndexOutOfBoundsException e){
+							System.out.println("Bad stuff happening");
+							System.out.println(e);
+							System.out.println("Array:" + lineParts);
+							System.out.println("Source string: '" + snl + "'");
+							//System.out.println("next line: " + s.hasNextLine());
+							//System.out.println("next line: " + s.nextLine());
+						}
+
+					} else if (code == 5) {
+						result.addLine(new OptionalLineSpec(lineParts, colors));
 					}
-					catch ( ArrayIndexOutOfBoundsException e){
-						System.out.println("Bad stuff happening");
-						System.out.println(e);
-						System.out.println("Array:" + lineParts);
-						System.out.println("Source string: '" + snl + "'");
-						System.out.println("next line: " + s.hasNextLine());
-						System.out.println("next line: " + s.nextLine());
-					}
-					
-				} else if (code == 5) {
-					result.addLine(new OptionalLineSpec(lineParts, colors));
 				}
 			}
+			reader.close();
+		} catch (NumberFormatException e) {
+			System.out.println("numberFormatException, line 116 partsfactory.java.");
+			System.out.println("snl: " + snl);
+			result.debugging();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOexception, line 121 partsfactory.java.");
+			System.out.println("snl: " + snl);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		s.close();
+
 		return result;
 	}
 }
