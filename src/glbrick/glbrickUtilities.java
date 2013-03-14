bb96cbd2f9b7c560ddd1c32be77f52e22498008c
John D <tanpantsman@gmail.com>
2013-03-14 23:47:59 +0100

    utilities

---
+++ src/glbrick/glbrickUtilities.java
@@ -0,0 +1,32 @@
+package glbrick;
+
+import java.util.ArrayList;
+
+public class glbrickUtilities {
+	
+	public static float smartDecode(String s){
+		float ret;
+		if (s.length() >1 && s.contains("x")){
+			ret = (float) Integer.decode(s);
+		}
+
+		else{
+			ret = Float.parseFloat(s);
+		}
+		return ret;
+	}
+	
+	
+	public static String stringer(ArrayList<double[]> vertices){
+		
+		String printstr = "";
+		for(double[] d : vertices){
+			for(double e : d){
+				printstr += e + " ";
+			}
+		}
+		return printstr;
+
+	}
+
+}
