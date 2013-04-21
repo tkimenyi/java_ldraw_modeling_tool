75a6c8335d928f7549635e5ec3bd19d237b6d5f9
John D <tanpantsman@gmail.com>
2013-04-21 16:21:05 -0500

    remove ugly characters

--- src/glbrick/PartLabel.java
+++ src/glbrick/PartLabel.java
@@ -7,7 +7,13 @@ public class PartLabel extends JLabel{
 	private String partName;
 	private String partFile;
 	private String partIcon;
+	private final String ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	
 	public PartLabel(String partName, String partFile, String partIcon){
+		//removes ugly characters in parts bin
+		if(!ALPHA.contains(String.valueOf(partName.charAt(0)))){
+			partName = partName.substring(1);
+		}
 		this.partFile = partFile;
 		this.partIcon = partIcon;
 		this.partName = partName;
