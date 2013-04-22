169a179ba0e6e243aab8406ad9f7d06e5f206a56
Thierry <Thierry@localhost>
2013-04-21 23:31:59 -0500

    can translate individual parts, rotation needs to be fixed soon

--- src/junittest/Tests.java
+++ src/junittest/Tests.java
@@ -7,29 +7,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import glbrick.BrickSpec;
-import glbrick.ColorBase;
-import glbrick.GLWindowTest;
-import glbrick.GuInterface;
-import glbrick.LineSpec;
 import glbrick.PartNotFoundException;
-import glbrick.QuadSpec;
-import glbrick.TriangleSpec;
-import glbrick.ldrawParseTest;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.lwjgl.LWJGLException;
 
 public class Tests
 {
 
-
-	//put test back!!!!
-
-
-	public void test()
+	public void test() throws brick.PartNotFoundException
 	{
 		try
 		{
@@ -47,21 +33,14 @@ public class Tests
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+	
 	}
 
-	@Ignore
-	@Test
+
+
 	public void allPartTest() throws IOException
 	{
-		//test succeeds if it doesn't crash
-
-		/*
-		for (int i = 1; i <9000; i++){
-			ldrawParseTest ldp = new ldrawParseTest();
-			String temp = "";
-			temp += i + ".dat";*/
-		//System.out.println(temp);
+		
 		ArrayList<String> files = getFiles();
 		String finaloutPut = "";
 		System.out.println("test?");
@@ -78,7 +57,7 @@ public class Tests
 		}
 		System.out.println(finaloutPut);
 	}
-	
+
 	@Test
 	public void someParts() throws IOException
 	{
@@ -99,11 +78,6 @@ public class Tests
 			}
 			finaloutPut = finaloutPut + i++ +"\n";
 		}
-		System.out.println(finaloutPut);
-
-		System.out.println("did this run?");
-		guiTest gt = new guiTest();
-		//test();
 	}
 
 
@@ -111,8 +85,7 @@ public class Tests
 
 	public ArrayList<String> getFiles() throws IOException{
 		System.out.println("getfiles()");
-		String path = "/export/home/f09/dyerjw/eclipse-work/java-ldraw-cad/ldraw/parts.txt";
-		//String path = "/home/john/workspace/java-ldraw-cad/ldraw/parts.txt";
+		String path = "/home/john/workspace/java-ldraw-cad/ldraw/parts.txt";
 		String fileName = path;
 		ArrayList<String> allFiles = new ArrayList<String>(7118);
 
@@ -130,14 +103,6 @@ public class Tests
 
 		reader.close();
 
-
-
-		/*Scanner s = new Scanner(new File(filename));
-	int i = 0;
-	while (s.hasNextLine()) {
-		allFiles.add(s.nextLine());
-
-	}*/
 		return allFiles;
 
 	}
@@ -148,103 +113,33 @@ public class Tests
 	 */
 	public void test2() throws FileNotFoundException
 	{
-		//test succeeds if it doesn't crash
+
 		ldrawParseTest ldp = new ldrawParseTest();
 
 
 		try{
-			//assertTrue(ldp.lDrawTest("s/.dat"));
 			System.out.println("test");
 			assertTrue(ldp.lDrawTest("s/2902s01.dat"));
 			System.out.println("test2");
-			//assertTrue(ldp.lDrawTest("s/.dat"));
-
 		}
 		catch (PartNotFoundException e){
 		}
 	}
 
-	
+
 	public void test3() throws FileNotFoundException
 	{
-		//test succeeds if it doesn't crash
+
 		ldrawParseTest ldp = new ldrawParseTest();
-
-
 		try{
-			//assertTrue(ldp.lDrawTest("s/.dat"));
 			System.out.println("test");
 			assertTrue(ldp.lDrawTest("s/2902s01.dat"));
 			System.out.println("test2");
-			//assertTrue(ldp.lDrawTest("s/.dat"));
 
 		}
 		catch (PartNotFoundException e){
 			System.out.println(e);
 		}
-	}
-	@Test
-
-	public void quadTest(){
-		String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0","19.0","32.0","-10.0","19.0","29.0","-10.0"};
-		ColorBase cb;
-		try {
-			cb = new ColorBase("ldraw");
-			QuadSpec t = new QuadSpec(lp, cb);
-			specTest(lp,t);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			assertFalse(1==2);
-		}
-	}
-
-	@Test
-	public void triTest(){ 
-		String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0","19.0","32.0","-10.0"};
-		ColorBase cb;
-		try {
-			cb = new ColorBase("ldraw");
-			TriangleSpec t = new TriangleSpec(lp, cb);
-			specTest(lp,t);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			assertFalse(1==2);
-		}
-
-	}
-
-	@Test
-	public void lineTest(){
-		String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0"};
-		ColorBase cb;
-		try {
-			cb = new ColorBase("ldraw");
-			LineSpec t = new LineSpec(lp, cb);
-			specTest(lp,t);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			assertFalse(1==2);
-		}
-
-	}
-
-
-
-	public void specTest(String[] lp, BrickSpec b){
-		assertFalse(b.isCommment());
-		ArrayList<String> al = new ArrayList<String>();
-		for (String s: lp){
-			al.add(s);
-		}
-		for (double[] d: b.toDrawnObject().getVertices()){
-			for (double d2: d){
-				al.remove(String.valueOf(d2));
-			}
-		}
-		assertTrue(al.remove("4.0") && al.remove("16.0") && al.isEmpty());
 	}
 
 }
