fda92d882d404d4b8877da7b7e3240fc65a0dd35
safari sibomana <sibomanaas@hendrix.edu>
2013-04-22 00:10:25 -0500

    tests fixes

--- src/junittest/GLWindowTest.java
+++ src/junittest/GLWindowTest.java
@@ -2,11 +2,10 @@ package junittest;
 
 import java.io.FileNotFoundException;
 import org.lwjgl.LWJGLException;
-import brick.PartNotFoundException;
 import glbrick.GLWindow;
 
 public class GLWindowTest {
-	public static void main(String[] args) throws LWJGLException, InterruptedException, PartNotFoundException, FileNotFoundException, glbrick.PartNotFoundException
+	public static void main(String[] args) throws LWJGLException, InterruptedException, FileNotFoundException, glbrick.PartNotFoundException
     {
             GLWindow window = new GLWindow();
             window.run();
