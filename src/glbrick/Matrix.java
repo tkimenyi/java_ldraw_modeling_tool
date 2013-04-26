86e792502718e4bc30551d46a4527b4301e7db5f
Thierry <Thierry@localhost>
2013-04-26 10:07:48 -0500

    even more testing

--- src/glbrick/Matrix.java
+++ src/glbrick/Matrix.java
@@ -2,8 +2,7 @@ package glbrick;
 
 public  class Matrix {
 	
-	public static double[] matrixMult(double[][] m, double[] v)
-	{
+	public static double[] matrixMult(double[][] m, double[] v){
 		double[] newv = new double[v.length];
 		newv[0] = v[0] * m[0][0] + v[1] * m[0][1] + v[2] * m[0][2];
 		newv[1] = v[0] * m[1][0] + v[1] * m[1][1] + v[2] * m[1][2];
@@ -11,13 +10,57 @@ public  class Matrix {
 		return newv;
 	}
 	
-	
 	public static double [][] matrixMult2(double[][] p, double[][] q){
-		double [] newp0 = matrixMult(p,q[0]);
-		double [] newp1 = matrixMult(p,q[1]);
-		double [] newp2 = matrixMult(p,q[2]);
 		
-		double [][] newp = {newp0,newp1,newp2};
+		double [][] newp  = new double [p.length][q.length];
+		for(int i = 0; i < p.length; i++) {
+			for(int j = 0; j < q.length; j++) {
+				for(int k = 0; k < q.length; k++){
+					newp[i][j] += p[i][k]*q[k][j];
+				}
+			}
+		}
 		return newp;
+	}
+	
+	public static double[] subtract(double[] v1, double[] v2)
+	{
+		double[] result = new double[v1.length];
+		for (int i = 0; i < result.length; i++)
+		{
+			result[i] = v1[i] - v2[i];
+		}
+		return result;
+	}
+	
+	public static double magnitude(double[] v)
+	{
+		double result = 0;
+		for (double i : v)
+		{
+			result += i * i;
+		}
+		return Math.sqrt(result);
+	}
+
+	public static double[] normalize(double[] v)
+	{
+		double[] result = new double[v.length];
+		result[0] = v[0] / magnitude(v);
+		result[1] = v[1] / magnitude(v);
+		result[2] = v[2] / magnitude(v);
+		return result;
+	}
+
+	public static double[] cross_product(double[] v1, double[] v2)
+	{
+		double[] result = new double[v1.length];
+
+		result[0] = v1[1] * v2[2] - v1[2] * v2[1];
+		result[1] = v1[2] * v2[0] - v1[0] * v2[2];
+		result[2] = v1[0] * v2[1] - v1[1] * v2[0];
+
+		return result;
+
 	}
 }
--- src/junittest/Tests.java
+++ src/junittest/Tests.java
@@ -7,240 +7,286 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import glbrick.BrickSpec;
 import glbrick.ColorBase;
-import glbrick.GLWindowTest;
+import glbrick.DrawnObject;
 import glbrick.GuInterface;
 import glbrick.LineSpec;
+import glbrick.Matrix;
 import glbrick.PartNotFoundException;
 import glbrick.QuadSpec;
 import glbrick.TriangleSpec;
-import glbrick.ldrawParseTest;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.lwjgl.LWJGLException;
 
 public class Tests
 {
+	private DrawnObject obj;
+	
+	@Before
+	public void initialize(){
+		obj = new DrawnObject();
+	}
+	
+	@Test
+	public void test() throws IOException, LWJGLException, 
+					InterruptedException, PartNotFoundException{
+	    	    GLWindowTest.main(new String[]{});
+	}
+	
+	@Ignore //this is used by allPartsTest
+	public ArrayList<String> getFiles() throws IOException{
+        String path = "/export/home/f09/dyerjw/eclipse-work/java-ldraw-cad/ldraw/parts.txt";
+        String fileName = path;
+        ArrayList<String> allFiles = new ArrayList<String>(7118);
 
 
-        //put test back!!!!
+        FileReader fr = new FileReader(fileName);
+        BufferedReader reader = new BufferedReader(fr);
 
 
-        public void test()
-        {
-                try
-                {
-                        GLWindowTest.main(new String[]{});
-                } catch (InterruptedException e)
-                {
-                        e.printStackTrace();
-                } catch (FileNotFoundException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                } catch (PartNotFoundException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                } catch (LWJGLException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                }
+        String str = reader.readLine();
+        while (reader.ready()){
+                allFiles.add(str);
+                str = reader.readLine();
 
         }
 
-        @Ignore
-        @Test
-        public void allPartTest() throws IOException
-        {
-                //test succeeds if it doesn't crash
+        reader.close();
+        return allFiles;
+    }
 
-                /*
-                for (int i = 1; i <9000; i++){
-                        ldrawParseTest ldp = new ldrawParseTest();
-                        String temp = "";
-                        temp += i + ".dat";*/
-                //System.out.println(temp);
-                ArrayList<String> files = getFiles();
-                String finaloutPut = "";
-                System.out.println("test?");
-                int i = 0;
-                ldrawParseTest ldp = new ldrawParseTest();
-                for(String s : files){
-                        finaloutPut = finaloutPut + s +"\n";
-                        try{
-                                assertTrue(ldp.lDrawTest(s));
-                        }
-                        catch (PartNotFoundException e){
-                        }
-                        finaloutPut = finaloutPut + i++ +"\n";
-                }
-                System.out.println(finaloutPut);
-        }
-        
-        @Test
-        public void someParts() throws IOException
-        {
-                //test succeeds if it doesn't crash
-                ArrayList<String> files = new ArrayList<String>(1);
-                files.add("3003.dat");
-                String finaloutPut = "";
-                System.out.println("test?");
-                int i = 0;
-                ldrawParseTest ldp = new ldrawParseTest();
-                for(String s : files){
-                        finaloutPut = finaloutPut + s +"\n";
-                        try{
-                                assertTrue(ldp.lDrawSingleTest(s));
-
-                        }
-                        catch (PartNotFoundException e){
-                        }
-                        finaloutPut = finaloutPut + i++ +"\n";
-                }
-        }
-
-
-
-
-        public ArrayList<String> getFiles() throws IOException{
-                System.out.println("getfiles()");
-                String path = "/export/home/f09/dyerjw/eclipse-work/java-ldraw-cad/ldraw/parts.txt";
-                //String path = "/home/john/workspace/java-ldraw-cad/ldraw/parts.txt";
-                String fileName = path;
-                ArrayList<String> allFiles = new ArrayList<String>(7118);
-
-
-                FileReader fr = new FileReader(fileName);
-                BufferedReader reader = new BufferedReader(fr);
-
-
-                String str = reader.readLine();
-                while (reader.ready()){
-                        allFiles.add(str);
-                        str = reader.readLine();
-
-                }
-
-                reader.close();
-
-
-
-                /*Scanner s = new Scanner(new File(filename));
+    @Ignore // It takes too long to run, check testSomeParts() for a shorter version
+    @Test
+    public void allPartTest() throws IOException
+    {
+        ArrayList<String> files = getFiles();
+        String finaloutPut = "";
+        System.out.println("test?");
         int i = 0;
-        while (s.hasNextLine()) {
-                allFiles.add(s.nextLine());
-
-        }*/
-                return allFiles;
-
-        }
-
-
-        /**
-         * @throws FileNotFoundException
-         */
-        public void test2() throws FileNotFoundException
-        {
-                //test succeeds if it doesn't crash
-                ldrawParseTest ldp = new ldrawParseTest();
-
-
+        ldrawParseTest ldp = new ldrawParseTest();
+        for(String s : files){
+                finaloutPut = finaloutPut + s +"\n";
                 try{
-                        //assertTrue(ldp.lDrawTest("s/.dat"));
-                        System.out.println("test");
-                        assertTrue(ldp.lDrawTest("s/2902s01.dat"));
-                        System.out.println("test2");
-                        //assertTrue(ldp.lDrawTest("s/.dat"));
-
+                        assertTrue(ldp.lDrawTest(s));
                 }
                 catch (PartNotFoundException e){
                 }
+                finaloutPut = finaloutPut + i++ +"\n";
         }
-
+        System.out.println(finaloutPut);
+    }
         
-        public void test3() throws FileNotFoundException
-        {
-                //test succeeds if it doesn't crash
-                ldrawParseTest ldp = new ldrawParseTest();
+    @Test
+    public void testSomeParts() throws IOException, PartNotFoundException{
+        //test succeeds if it doesn't crash
+        ArrayList<String> files = new ArrayList<String>(1);
+        files.add("3003.dat");
+        String finaloutPut = "";
+        System.out.println("test?");
+        int i = 0;
+        ldrawParseTest ldp = new ldrawParseTest();
+        for(String s : files){
+        	finaloutPut = finaloutPut + s +"\n";
+        	assertTrue(ldp.lDrawSingleTest(s));
+            finaloutPut = finaloutPut + i++ +"\n";
+        }
+    }
 
+    
+    
+    @Test
+    public void test2() throws FileNotFoundException, PartNotFoundException
+    {
+        ldrawParseTest ldp = new ldrawParseTest();
+		System.out.println("test");
+		assertTrue(ldp.lDrawTest("s/2902s01.dat"));
+		System.out.println("test2");
+       
+    }
 
-                try{
-                        //assertTrue(ldp.lDrawTest("s/.dat"));
-                        System.out.println("test");
-                        assertTrue(ldp.lDrawTest("s/2902s01.dat"));
-                        System.out.println("test2");
-                        //assertTrue(ldp.lDrawTest("s/.dat"));
+    @Test   
+    public void test3() throws FileNotFoundException, PartNotFoundException
+    {
+        //test succeeds if it doesn't crash
+        ldrawParseTest ldp = new ldrawParseTest();
+	    System.out.println("test");
+	    assertTrue(ldp.lDrawTest("s/2902s01.dat"));
+	    System.out.println("test2");
+    }
+    
+    @Test
+    public void quadTest(){
+	    String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0","19.0","32.0","-10.0","19.0","29.0","-10.0"};
+	    ColorBase cb;
+	    try {
+            cb = new ColorBase("ldraw");
+            QuadSpec t = new QuadSpec(lp, cb);
+            specTest(lp,t);
+	    } catch (FileNotFoundException e) {
+	        fail("quadtest failed, file was not found");
+	    }
+    }
 
-                }
-                catch (PartNotFoundException e){
-                        System.out.println(e);
+    @Test
+    public void triTest(){ 
+	    String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0","19.0","32.0","-10.0"};
+	    ColorBase cb;
+	    try {
+            cb = new ColorBase("ldraw");
+            TriangleSpec t = new TriangleSpec(lp, cb);
+            specTest(lp,t);
+	    } catch (FileNotFoundException e) {
+	    	fail("tritest failed, file was not found");
+	    }
+
+    }
+
+    @Test
+    public void lineTest(){
+        String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0"};
+        ColorBase cb;
+        try {
+            cb = new ColorBase("ldraw");
+            LineSpec t = new LineSpec(lp, cb);
+            specTest(lp,t);
+        } catch (FileNotFoundException e) {
+        	fail("linetest failed, file was not found");
+        }
+
+    }
+    
+    public void specTest(String[] lp, BrickSpec b){
+        assertFalse(b.isCommment());
+        ArrayList<String> al = new ArrayList<String>();
+        for (String s: lp){
+                al.add(s);
+        }
+        for (double[] d: b.toDrawnObject().getVertices()){
+                for (double d2: d){
+                        al.remove(String.valueOf(d2));
                 }
         }
-        @Test
-
-        public void quadTest(){
-                String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0","19.0","32.0","-10.0","19.0","29.0","-10.0"};
-                ColorBase cb;
-                try {
-                        cb = new ColorBase("ldraw");
-                        QuadSpec t = new QuadSpec(lp, cb);
-                        specTest(lp,t);
-                } catch (FileNotFoundException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                        assertFalse(1==2);
-                }
-        }
-
-        @Test
-        public void triTest(){ 
-                String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0","19.0","32.0","-10.0"};
-                ColorBase cb;
-                try {
-                        cb = new ColorBase("ldraw");
-                        TriangleSpec t = new TriangleSpec(lp, cb);
-                        specTest(lp,t);
-                } catch (FileNotFoundException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                        assertFalse(1==2);
-                }
-
-        }
-
-        @Test
-        public void lineTest(){
-                String[] lp = new String[]{"4.0","16.0","17.125","18.078","-10.0","17.125","32.0","-10.0"};
-                ColorBase cb;
-                try {
-                        cb = new ColorBase("ldraw");
-                        LineSpec t = new LineSpec(lp, cb);
-                        specTest(lp,t);
-                } catch (FileNotFoundException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                        assertFalse(1==2);
-                }
-
-        }
-
-
-
-        public void specTest(String[] lp, BrickSpec b){
-                assertFalse(b.isCommment());
-                ArrayList<String> al = new ArrayList<String>();
-                for (String s: lp){
-                        al.add(s);
-                }
-                for (double[] d: b.toDrawnObject().getVertices()){
-                        for (double d2: d){
-                                al.remove(String.valueOf(d2));
-                        }
-                }
-                assertTrue(al.remove("4.0") && al.remove("16.0") && al.isEmpty());
-        }
+        assertTrue(al.remove("4.0") && al.remove("16.0") && al.isEmpty());
+    }
+    
+    public void printMatrix(double [] p){
+    	for(int i=0;i<p.length;i++){
+    		System.out.println(p[i]);
+    	}
+    }
+    
+    public void print2DMatrix(double [][] p){
+    	for(int i=0;i<p.length;i++){
+    		for(int j=0;j<p[i].length;j++){
+    			System.out.print(p[i][j]+"\t");
+    			
+    		}
+    		System.out.println("\n");
+    	}
+    	System.out.println();
+    }
+    
+    @Test
+    public void testMatrixMultiplication(){
+    	double [][] p = {{1,2,3},{3,4,7},{10,3,3}};
+    	double [] q = {1,4,5};
+    	double [][] m = {{-1,-3,3},{2,-4,5},{1,-3,3}};
+    	
+    	print2DMatrix(p);
+    	print2DMatrix(m);
+    	System.out.println();
+    	
+    	double [] resp = Matrix.matrixMult(p, q);
+    	printMatrix(resp);
+    	double [] result = {24.0,54.0,37.0};
+    	assertTrue(Arrays.equals(resp,result));
+    	
+    	double [][] resp2 = Matrix.matrixMult2(p, m);
+    	print2DMatrix(resp2);
+    	double [][] result2 = {{6.0,-20.0,22.0},{12.0,-46.0,50.0},{-1.0,-51.0,54.0}};
+    	assertTrue(Arrays.equals(resp2[0], result2[0]));
+    	assertTrue(Arrays.equals(resp2[1], result2[1]));
+    	assertTrue(Arrays.equals(resp2[2], result2[2]));
+    }
+    
+    @Test
+    public void testMatrixSubtraction(){
+    	double [] v= {1,2,3};
+    	double [] w= {-1,5,-6};
+    	double result []= {2,-3,9};
+    	assertTrue(Arrays.equals(Matrix.subtract(v, w),result));
+    }
+    
+    @Test
+    public void testMatrixMagnitude(){
+    	double [] r= {-1,5,-6};
+    	double result =Math.sqrt(62.0);
+    	assertEquals((int)Matrix.magnitude(r),(int)result);
+    	System.out.println("magnitude: "+ (int)Matrix.magnitude(r));
+    }
+    
+    @Test
+    public void testMatrixNormalize(){
+    	double [] r= {-2,4,-6};
+    	double magnitude =Matrix.magnitude(r);
+    	double [] normalR = {r[0]/magnitude , r[1]/magnitude, r[2]/magnitude};
+    	double [] otherNormalR = Matrix.normalize(r);
+    	assertTrue(Arrays.equals(normalR, otherNormalR));
+    	
+    }
+    
+    @Test
+    public void testMatrixCrossProduct(){
+    	double [] t= {10,2,3};
+    	double [] r = {12,4,5};
+    
+    	double [] res = new double[t.length];
+    	res[0] = t[1] * r[2] - t[2] * r[1];
+		res[1] = t[2] * r[0] - t[0] * r[2];
+		res[2] = t[0] * r[1] - t[1] * r[0];
+		
+		double[] crossP = Matrix.cross_product(t, r);
+		assertTrue(Arrays.equals(res, crossP));
+ 
+    }
+    
+    @Test
+    public void testDrawnObjectLocation(){
+    	double[] loc = {10.0, 20.0, -15.0};
+    	double[] to = {5,4,5};
+    	double[] at = {15.0,24.0,-10.0};
+    	obj.setLocation(loc);
+    	obj.move(to);
+    	assertFalse(Arrays.equals(obj.getLocation(), loc));
+    	assertTrue(Arrays.equals(obj.getLocation(),at));
+    	assertTrue(obj.getx() == at[0]);
+    	assertTrue(obj.gety() == at[1]);
+    	assertTrue(obj.getz() == at[2]);
+    }
+    
+    @Test
+    public void testDrawnObjectTransformation(){
+    	double [][] trans = {{-1,-3,3,0},{2,-4,5,0},{1,-3,3,0},{0,0,0,1}};
+    	double [][] t = obj.getTransformation();
+    	double [][] dt = obj.twoDArrayCopy(t);
+    	
+    	obj.setTransformation(trans);
+    	double [][] newTrans = obj.getTransformation();
+    	
+    	print2DMatrix(newTrans);
+    	
+    	assertTrue(Arrays.equals(trans[0], newTrans[0]));
+    	assertTrue(Arrays.equals(trans[1], newTrans[1]));
+    	assertTrue(Arrays.equals(trans[2], newTrans[2]));
+    }
+    
+   
 
 }
 
